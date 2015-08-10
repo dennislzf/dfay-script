a0d450845d2ef3191ec35787742ae742130a6da3
Dennis Lau <dennislzf@gmail.com>
2015-08-10 13:39:47 -0700

    fixed regexs

--- dfay.sh
+++ dfay.sh
@@ -2,25 +2,27 @@
 $FOLDER=""
 #if output doesnt exist, create it, if it exists then remove all the files
 if [ -d "output/" ]; then
-    rm output/*
+    rm -r output/
+    mkdir output
+
       # Control will enter here if $DIRECTORY exists.
   else
       mkdir output
   fi
-for LINE in `diff -r data1/ data2/ | grep st-.*\.jpg$`
+for LINE in `diff -r data1/ data2/ | grep st.*\.jpg`
     do
-    if [[ $LINE == *"/:" ]]
+    if [[ $LINE =~ .*: ]]
     then
         #folder is line without the last character as its a ':'
         FOLDER="${LINE%?}"
     fi
-
-    if [[ "$LINE" == *"st-"* ]]
+    if [[ $LINE =~ .*"st".*\.jpg ]];
     then
-        #concat folder and line
-        OUTPUTFILE=$FOLDER$LINE
-        cp $OUTPUTFILE output/$LINE
+      #concat folder and line
+      OUTPUTFILE=$FOLDER$LINE;
+      echo $OUTPUTFILE
+      cp $OUTPUTFILE output/$LINE;
 
     fi
-    echo "Diffs have been copied to output/"
 done
+echo "Diffs have been copied to output/"
