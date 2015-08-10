1d76ae33b8d7012fc0900e691dd215ac7d96a322
Dennis Lau <dennislzf@gmail.com>
2015-08-10 12:39:37 -0700

    changed variable name to uppercase

--- dfay.sh
+++ dfay.sh
@@ -1,14 +1,14 @@
 #i/bin/bash
 $FOLDER=""
-#if output doesnt exist, create it, if it exists then remove all the files 
+#if output doesnt exist, create it, if it exists then remove all the files
 if [ -d "output/" ]; then
     rm output/*
       # Control will enter here if $DIRECTORY exists.
   else
       mkdir output
   fi
-for LINE in `diff -r compare1/ compare2/ | grep st-.*\.jpg$`
-do
+for LINE in `diff -r data1/ data2/ | grep st-.*\.jpg$`
+    do
     if [[ $LINE == *"/:" ]]
     then
         #folder is line without the last character as its a ':'
@@ -16,13 +16,11 @@ do
     fi
 
     if [[ "$LINE" == *"st-"* ]]
-    then  
+    then
         #concat folder and line
         OUTPUTFILE=$FOLDER$LINE
-        cp $outputfile output/$LINE
+        cp $OUTPUTFILE output/$LINE
 
     fi
     echo "Diffs have been copied to output/"
 done
-
-
